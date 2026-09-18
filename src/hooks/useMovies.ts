2e2b35f5f412f85276ecdbb0e3a4a85d28822eff
Ahmed Mamdouh <mamdouhahm677@gmail.com>
2026-09-19 00:25:14 +0300

    fix: resolve discover movies not loading

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 export function useMovies(key: string, queryFn: any) {
   return useQuery<any>({
     queryKey: [key],
-    queryFn: queryFn,
+    queryFn: () => queryFn(),
     staleTime: 1000 * 60 * 5,
   });
 }
